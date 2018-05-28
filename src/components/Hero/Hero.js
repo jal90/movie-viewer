ac1c26e88d8fd83528049a1d55e28474f02da83d
jal90 <mezzyjezze@gmail.com>
2018-05-28 17:49:12 -0400

    Add hero section
    Add hero section

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+
+class Hero extends Component {
+
+  render() {
+    return (
+      <div>
+        <section className="hero is-primary is-bold">
+          <div className="hero-body">
+            <div className="container">
+              <h1 className="title">
+                A Journey into New Technologies
+              </h1>
+              <h2 className="subtitle">
+                Be amazed at my first creation with React and Bulma
+              </h2>
+            </div>
+          </div>
+        </section>
+      </div>
+    )
+  }
+}
+
+export default Hero;
